42a06d3169783747c128e8a46a5299a1b5511eba
Eray Erdin <eraygezer.94@gmail.com>
2023-06-07 23:07:23 +0300

    initial commit

---
+++ lib/get_it_future_builder.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library get_it_future_builder;
+
+export 'src/get_it_future_builder.dart';
---
+++ lib/src/get_it_future_builder.dart
@@ -0,0 +1,7 @@
+/// {@template get_it_future_builder}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class GetItFutureBuilder {
+  /// {@macro get_it_future_builder}
+  const GetItFutureBuilder();
+}
