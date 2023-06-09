c3668135d59fbc6b366af6446e09b5d77400d547
Eray Erdin <eraygezer.94@gmail.com>
2023-06-09 11:49:56 +0300

    implement GetItFutureBuilder widget

--- lib/src/get_it_future_builder.dart
+++ lib/src/get_it_future_builder.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/widgets.dart';
-import 'package:get_it_future_builder/src/dependency.dart';
+import 'package:get_it/get_it.dart';
 
 /// {@template get_it_future_builder}
 /// A builder widget for async dependencies registered to `get_it`.
 /// {@endtemplate}
 // ignore: must_be_immutable
-class GetItFutureBuilder<T> extends StatelessWidget {
+class GetItFutureBuilder<T extends Object> extends StatelessWidget {
   /// {@macro get_it_future_builder}
   GetItFutureBuilder({
-    required GetItDependency dependency,
     required Widget Function(BuildContext context) loading,
     required Widget Function(BuildContext context, T instance) ready,
     super.key,
+    String? instanceName,
   }) {
-    _dependency = dependency;
+    _instanceName = instanceName;
     _loading = loading;
     _ready = ready;
   }
 
-  late GetItDependency _dependency;
+  late String? _instanceName;
   late Widget Function(BuildContext context) _loading;
   late Widget Function(BuildContext context, T instance) _ready;
 
   @override
   Widget build(BuildContext context) {
-    return const Text('hello');
+    return FutureBuilder(
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return _ready(context, snapshot.data!);
+        } else {
+          return _loading(context);
+        }
+      },
+      future: () async {
+        final dependency =
+            await GetIt.I.getAsync<T>(instanceName: _instanceName);
+        return dependency;
+      }(),
+    );
   }
 }
